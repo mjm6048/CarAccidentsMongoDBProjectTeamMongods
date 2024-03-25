9bed8cddb6b937fe1101cc64628bb04c7a4b6817
JohnIII3 <jga4809@g.rit.edu>
2024-03-25 14:48:33 -0400

    added vehicle accidents dataset (200 record version), created db_populate.bash, deleted old dataset

---
+++ db_populate.bash
@@ -0,0 +1,2 @@
+mongosh 127.0.0.1:27017 --quiet --eval "use mongoProject" --eval "db.accidents.drop()"
+mongoimport --db mongoProject --collection accidents --type csv --headerline --file US_Accidents_March23_Transformed_200.csv
