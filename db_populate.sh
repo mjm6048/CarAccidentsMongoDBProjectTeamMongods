4990f6ad3b17aec7e5f9c2a7f397e28af7502533
Miles Krassen <msk4480@rit.edu>
2024-03-25 20:32:56 -0400

    changed populate over to full dataset

--- db_populate.sh
+++ db_populate.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/bash
 
 mongosh 127.0.0.1:27017 --quiet --eval "use mongoProject" --eval "db.accidents.drop()"
-mongoimport --db mongoProject --collection accidents --type csv --headerline --file US_Accidents_March23_Transformed_200.csv
+mongoimport --db mongoProject --collection accidents --type csv --headerline --file US_Accidents_March23_Transformed.csv
