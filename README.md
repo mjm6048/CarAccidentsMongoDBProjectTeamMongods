6010670f9a962b3fa62ac929c867e95818cc3ae9
Miles Krassen <msk4480@rit.edu>
2024-04-01 01:56:12 -0400

    updated database fields and dataset, added README

---
+++ README.md
@@ -0,0 +1,10 @@
+# Mongo Project
+Miles, John, Michael, Natnail
+
+## Start the application
+```shell
+pm2 start
+```
+This will run the `npm start` command and run it as a background process. To stop the process, type `pm2 kill`.
+
+
--- db_populate.sh
+++ db_populate.sh
@@ -1,4 +1,6 @@
 #! /usr/bin/bash
 
-mongosh 127.0.0.1:27017 --quiet --eval "use mongoProject" --eval "db.accidents.drop()"
+mongosh 127.0.0.1:27017 --quiet --eval "use mongoProject" --eval "db.accidents.drop()" --eval "db.users.drop()" --eval "db.images.drop()"
 mongoimport --db mongoProject --collection accidents --type csv --headerline --file US_Accidents_March23_Transformed.csv
+# add import for images collection from dump
+mongoimport --db mongoProject --collection users --type csv --headerline --file Users.csv
