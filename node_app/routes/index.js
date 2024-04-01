5ca1b8dea7914cbff6779e300df4a9790499b463
Miles Krassen <msk4480@rit.edu>
2024-04-01 14:34:13 -0400

    testing out jade and mongodb connection

--- node_app/routes/index.js
+++ node_app/routes/index.js
@@ -1,7 +1,9 @@
+const { ObjectId, Decimal128 } = require('mongodb');
+
 const express = require( 'express' ),
       mongoose = require( 'mongoose' ),
       router = express.Router(),
-      url = `mongodb://127.0.0.1:27017/siteInfo`;
+      url = `mongodb://127.0.0.1:27017/mongoProject`;
       
 // establish database connection
 mongoose.connect( url )
@@ -12,20 +14,45 @@ mongoose.connect( url )
   console.error('Error connecting to MongoDB:', error);
 });
 
-// build data model
-const siteDefaultsSchema = new mongoose.Schema({
-  _id: Number,
-  siteName: String
+const accidentsSchema = new mongoose.Schema({
+  _id: ObjectId,
+  Severity: Number,
+  Start_Time: Date,
+  End_Time: Date,
+  Start_Lat: Decimal128,
+  Start_Lng: Decimal128,
+  Description: String,
+  Street: String,
+  City: String,
+  County: String,
+  State: String,
+  Zipcode: String,
+  Country: String,
+  Timezone: String,
+  Weather_Timestamp: Date,
+  Weather_Condition: String,
+  Comments: String
 });
-const SiteDefaults = mongoose.model( 'defaults', siteDefaultsSchema );
+
+const accidents = mongoose.model('accidents', accidentsSchema);
+
+// build data model
+// const siteDefaultsSchema = new mongoose.Schema({
+//   _id: Number,
+//   siteName: String
+// });
+// const SiteDefaults = mongoose.model( 'defaults', siteDefaultsSchema );
 
 // GET home page
 router.get('/', async ( req, res ) => { 
       
   try {
-    const defaultsData = await SiteDefaults.findOne( { _id: 1 } );
-    console.log( defaultsData );
-    res.render( `index`, { title: defaultsData.siteName });
+    // const defaultsData = await SiteDefaults.findOne( { _id: 1 } );
+    // console.log( defaultsData );
+    // res.render( `index`, { title: defaultsData.siteName });
+
+    const test = await accidents.find({}, {Description: 1}).limit(5);
+    res.render(`index`, {title: test});
   }
   catch ( error ) {
     console.error( error );
