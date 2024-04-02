03f0efc09f1a75a2a8fa8912ee97b868c596920e
Michael M <mjm6048@rit.edu>
2024-04-02 02:42:47 -0400

    Command to create comments array in the database

--- db_populate.sh
+++ db_populate.sh
@@ -6,6 +6,9 @@
 
 mongosh 127.0.0.1:27017 --quiet --eval "use mongoProject" --eval "db.accidents.drop()" --eval "db.users.drop()" --eval "db.images.drop()"
 mongoimport --db mongoProject --collection accidents --type csv --headerline --file US_Accidents_March23_Transformed_200.csv
+# This next commands needs to be run immediately after import. This will add an actual comments array to the database.
+# There is few workarounds for an empty array in csv import so we have to do it this way
+db.accidents.updateMany({}, {$set : {"Comments":[]}}, {upsert:false, multi:true})
 # add import for images collection from dump
 mongoimport --db mongoProject --collection users --type csv --headerline --file Users.csv
 
