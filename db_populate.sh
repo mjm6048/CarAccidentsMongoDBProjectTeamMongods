d1a833afc3024583b1c995e72748feb93bef31ea
JohnIII3 <89408773+JohnIII3@users.noreply.github.com>
2024-04-02 01:32:36 -0400

    fixedgridfs image import, adding importImage.py

--- db_populate.sh
+++ db_populate.sh
@@ -8,27 +8,5 @@ mongoimport --db mongoProject --collection users --type csv --headerline --file 
 #gridfs image import, create bucket
 #mongo mongoProject importImages.js
 
-# Array of weather conditions
-weather_conditions=(
-    "Light Rain" "Overcast" "Mostly Cloudy" "Rain" "Light Snow"
-    "Haze" "Scattered Clouds" "Partly Cloudy" "Clear" "Snow"
-    "Light Freezing Drizzle" "Light Drizzle" "Fog"
-)
-
-# Loop through each weather condition
-for condition in "${weather_conditions[@]}"; do
-    # Form image path
-    image_path="./images/${condition}.jpg"
-
-    # Insert image into database using mongofiles
-    mongofiles --db mongoProject put "$image_path" --local "$image_path"
-
-    # Update corresponding documents in accidents collection
-    mongo 127.0.0.1:27017/mongoProject --quiet --eval "
-        db.accidents.update(
-            { Weather_Condition: '$condition' },
-            { \$set: { Image: '$image_path' } },
-            { multi: true }
-        )
-    "
-done
+# Import images into GridFS
+python importImage.py
