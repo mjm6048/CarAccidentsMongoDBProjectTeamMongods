7f4d7b1a47d54966be32fdb81713fe6548d41b14
Michael M <mjm6048@rit.edu>
2024-04-02 01:57:10 -0400

    Instructions to drop and remove database for testing

--- db_populate.sh
+++ db_populate.sh
@@ -1,5 +1,9 @@
 #! /usr/bin/bash
 
+# If you need to reset your data for any reason, run this command below
+# use mongoProject;
+# db.dropDatabase();
+
 mongosh 127.0.0.1:27017 --quiet --eval "use mongoProject" --eval "db.accidents.drop()" --eval "db.users.drop()" --eval "db.images.drop()"
 mongoimport --db mongoProject --collection accidents --type csv --headerline --file US_Accidents_March23_Transformed_200.csv
 # add import for images collection from dump
