b05e2f48814b9c24ec10d3373e70ba6f452f166d
Michael M <mjm6048@rit.edu>
2024-04-02 04:00:55 -0400

    Now a one line simplified script to create and populate the comments array by calling createCommentsArray.js

--- db_populate.sh
+++ db_populate.sh
@@ -1,16 +1,15 @@
 #! /usr/bin/bash
 
-# If you need to reset your data for any reason, run this command below
-# use mongoProject;
-# db.dropDatabase();
-
+# If you need to reset your data for any reason, run this command below.
 mongosh 127.0.0.1:27017 --quiet --eval "use mongoProject" --eval "db.accidents.drop()" --eval "db.users.drop()" --eval "db.images.drop()"
+
+# Importing and loading the data from the csv.
 mongoimport --db mongoProject --collection accidents --type csv --headerline --file US_Accidents_March23_Transformed_200.csv
-# This next commands needs to be run immediately after import. This will add an actual comments array to the database.
-# There is few workarounds for an empty array in csv import so we have to do it this way
-mongosh
-use mongoProject;
-db.accidents.updateMany({}, {$set : {"Comments":[]}}, {upsert:false, multi:true})
+
+# This next command needs to be run immediately after import. This will add an actual comments array to the database.
+# There is very few workarounds for an empty array in csv import so we have to do it this way.
+mongosh 127.0.0.1:27017 --quiet createCommentsArray.js
+
 # add import for images collection from dump
 mongoimport --db mongoProject --collection users --type csv --headerline --file Users.csv
 
