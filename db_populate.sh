4d28ed013e3d3ac897d280ba0f3cc1a449b919f9
JohnIII3 <89408773+JohnIII3@users.noreply.github.com>
2024-04-01 20:28:21 -0400

    adding .js file for importing images and adding line to call the new .js file in db_populate.sh'

--- db_populate.sh
+++ db_populate.sh
@@ -4,3 +4,6 @@ mongosh 127.0.0.1:27017 --quiet --eval "use mongoProject" --eval "db.accidents.d
 mongoimport --db mongoProject --collection accidents --type csv --headerline --file US_Accidents_March23_Transformed.csv
 # add import for images collection from dump
 mongoimport --db mongoProject --collection users --type csv --headerline --file Users.csv
+
+#gridfs image import, create bucket
+mongo mongoProject importImages.js
