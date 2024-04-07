7f5e9c91c30c8f52e78e88b24485c29e7b50c843
Miles Krassen <msk4480@rit.edu>
2024-04-07 05:41:46 -0400

    added optional fix for images import

--- db_populate.sh
+++ db_populate.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/bash
 
 # If you need to reset your data for any reason, run this command below.
-mongosh 127.0.0.1:27017 --quiet --eval "use mongoProject" --eval "db.accidents.drop()" --eval "db.users.drop()" --eval "db.images.drop()"
+mongosh 127.0.0.1:27017 --quiet --eval "use mongoProject" --eval "db.accidents.drop()" --eval "db.users.drop()" --eval "db.fs.files.drop()" --eval "db.fs.chunks.drop()"
 
 # Importing and loading the data from the csv.
-mongoimport --db mongoProject --collection accidents --type csv --headerline --file US_Accidents_March23_Transformed_200.csv
+mongoimport --db mongoProject --collection accidents --type csv --headerline --file US_Accidents_March23_Transformed.csv
 
 # This next command needs to be run immediately after import. This will add an actual comments array to the database.
 # There is very few workarounds for an empty array in csv import so we have to do it this way.
@@ -25,3 +25,6 @@ mongoimport --db mongoProject --collection users --type csv --headerline --file 
 
 # Import images into GridFS
 python importImage.py
+
+# Fix for images -miles
+# ls /home/student/mongo-project-mongods2/images | awk '{print "mongofiles --db mongoProject put \""$0"\" --local \"/home/student/mongo-project-mongods2/images/"$0"\" --type \"image/jpg\""}' | sh
